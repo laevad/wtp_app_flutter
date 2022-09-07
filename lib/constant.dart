d5ad44c00be2ecfad02c2c5175d9c1f5033d8b88
laevad <djaldave1@gmail.com>
2022-09-07 09:45:23 +0800

    init

---
+++ lib/constant.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class Constant{
+   static ColorScheme lightColorScheme = const ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFFf4325c),
+    onPrimary: Color(0xFFFFFFFF),
+    primaryContainer: Color(0xFFFFD9E0),
+    onPrimaryContainer: Color(0xFF3F0016),
+    secondary: Color(0xFFC5A9B4),
+    onSecondary: Color(0xFFFFFFFF),
+    secondaryContainer: Color(0xFFFFD9E0),
+    onSecondaryContainer: Color(0xFF2B151A),
+    tertiary: Color(0xFF9299A5),
+    onTertiary: Color(0xFFFFFFFF),
+    tertiaryContainer: Color(0xFFFFDCB9),
+    onTertiaryContainer: Color(0xFF2C1600),
+    error: Color(0xFFBA1B1B),
+    errorContainer: Color(0xFFFFDAD4),
+    onError: Color(0xFFFFFFFF),
+    onErrorContainer: Color(0xFF410001),
+    background: Color(0xFFFCFCFC),
+    onBackground: Color(0xFF201A1B),
+    surface: Color(0xFFFCFCFC),
+    onSurface: Color(0xFF201A1B),
+    surfaceVariant: Color(0xFFF3DDE0),
+    onSurfaceVariant: Color(0xFF514345),
+    outline: Color(0xFF847376),
+    onInverseSurface: Color(0xFFFAEEEF),
+    inverseSurface: Color(0xFF362F30),
+    inversePrimary: Color(0xFFFFB1C3),
+    shadow: Color(0xFF000000),
+  );
+}
---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../constant.dart';
+
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+          colorScheme: Constant.lightColorScheme,
+          textTheme: GoogleFonts.openSansTextTheme().apply(
+              displayColor: const Color(0xFF383838),
+              bodyColor: const Color(0xFF383838)),
+          useMaterial3: true),
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(
+                parent: NeverScrollableScrollPhysics()),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('WT&P System',style: TextStyle(fontWeight: FontWeight.w600,color: Constant.lightColorScheme.primary),),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Welcome,',
+                                style: TextStyle(
+                                    fontSize: 34, fontWeight: FontWeight.w600),
+                              ),
+                              Text(
+                                'Sign in to continue!',
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w600,
+                                    color: Constant.lightColorScheme.tertiary),
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextFormField(
+                          validator: (val) {
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                              const BorderRadius.all(Radius.circular(14)),
+                              borderSide: BorderSide(
+                                  color:
+                                  Theme.of(context).colorScheme.tertiary),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 20),
+                            labelText: 'Username',
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        TextFormField(
+                          validator: (val) {
+                            return null;
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                              const BorderRadius.all(Radius.circular(14)),
+                              borderSide: BorderSide(
+                                  color:
+                                  Theme.of(context).colorScheme.tertiary),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 20),
+                            labelText: 'Password',
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              stops: [
+                                0.20,
+                                0.90,
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [
+                                Color(0xFFFB578E),
+                                Color(0xFFFDA58E),
+                              ],
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(14)),
+                          ),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              textStyle: const TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 18),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 20),
+                            ),
+                            onPressed: () {},
+                            child: const Text('Login', style: TextStyle(color: Colors.white),),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
