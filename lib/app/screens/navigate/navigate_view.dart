3664f5bb1e470c680d1d395200d7de3d51f0515e
laevad <djaldave1@gmail.com>
2022-09-29 09:32:36 +0800

    added navigateView

---
+++ lib/app/screens/navigate/navigate_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:wtp_app/app/screens/navigate/navigate_controller.dart';
+
+class NavigateView extends View {
+  const NavigateView({Key? key}) : super(key: key);
+
+  @override
+  NavigateViewState createState() => NavigateViewState();
+}
+
+class NavigateViewState extends ViewState<NavigateView, NavigateController> {
+  NavigateViewState() : super(NavigateController());
+
+  @override
+  Widget get view => ControlledWidgetBuilder<NavigateController>(
+        builder: (context, controller) {
+          return Scaffold(
+            key: globalKey,
+          );
+        },
+      );
+}
