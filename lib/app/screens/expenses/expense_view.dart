2212544d180096746aa9fa2d0aad2ac81d591bb4
laevad <djaldave1@gmail.com>
2022-10-12 10:31:52 +0800

    show bottom modal

--- lib/app/screens/expenses/expense_view.dart
+++ lib/app/screens/expenses/expense_view.dart
@@ -40,7 +40,31 @@ class ExpenseViewState extends ViewState<ExpenseView, ExpenseController> {
                   side: BorderSide(
                 color: Constant.lightColorScheme.onSecondaryContainer,
               )),
-              onPressed: () {},
+              onPressed: () {
+                showModalBottomSheet(
+                  enableDrag: true,
+                  backgroundColor: Colors.transparent,
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      height: 300,
+                      decoration: BoxDecoration(
+                          color: Constant.lightColorScheme.primaryContainer,
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25))),
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Close'),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
               child: const Icon(Icons.add),
             ),
           );
