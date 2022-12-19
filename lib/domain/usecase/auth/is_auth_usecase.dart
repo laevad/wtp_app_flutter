8c47650f9b9b45dafad9e03e20674aac206136da
laevad <aldave.tabian@ustp.edu.ph>
2022-12-19 11:28:53 +0800

    static url

--- lib/domain/usecase/auth/is_auth_usecase.dart
+++ lib/domain/usecase/auth/is_auth_usecase.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
+
 import '../../../data/repository/auth/data_auth_token_repository.dart';
+import '../../../data/repository/helpers/auth/is_auth.dart';
 import '../../repositories/login_details/auth_repository.dart';
 
 class IsAuthUseCase extends UseCase<IsAuthUseCaseResponse, void> {
@@ -13,6 +15,7 @@ class IsAuthUseCase extends UseCase<IsAuthUseCaseResponse, void> {
   Future<Stream<IsAuthUseCaseResponse?>> buildUseCaseStream(params) async {
     final controller = StreamController<IsAuthUseCaseResponse>();
     try {
+      await IsAuth.setToken(key: 'url', value:"https://wtp.web-wtp.online/api");
       bool isAuth = await DataAuthRepository().isAuthenticated();
       controller.add(IsAuthUseCaseResponse(isAuth));
       logger.finest('Is Authenticated successful.');
